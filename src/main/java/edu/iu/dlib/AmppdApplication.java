a62deab57d5487b3b71c489cc0bb4c24a7cc140a
yingfeng-iu <yingfeng@iu.edu>
2019-03-07 17:33:30 -0500

    AMP-36: skeleton spring boot project for amppd

---
+++ src/main/java/edu/iu/dlib/AmppdApplication.java
@@ -0,0 +1,13 @@
+package edu.iu.dlib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmppdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmppdApplication.class, args);
+	}
+
+}
---
+++ src/main/java/edu/iu/dlib/ServletInitializer.java
@@ -0,0 +1,13 @@
+package edu.iu.dlib;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AmppdApplication.class);
+	}
+
+}
