030e66b25ed0bb7394036aeb04aade119afb151a
vinitab <vinitab@bl-libg-ltdmsde.ads.iu.edu>
2019-04-17 14:14:26 -0400

    AMP-132_testsPass

---
+++ src/main/java/edu/iu/dlib/amppd/security/SecurityConfig.java
@@ -0,0 +1,25 @@
+package edu.iu.dlib.amppd.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity //Very important!
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {  
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception 
+	{
+		http.authorizeRequests().antMatchers("/").permitAll().and().authorizeRequests().antMatchers("/console/**").permitAll();
+	    http.authorizeRequests()
+	    	.anyRequest()
+	        .permitAll()
+	        .and().csrf().disable();
+	    http.headers().frameOptions().disable();
+	   }
+}
