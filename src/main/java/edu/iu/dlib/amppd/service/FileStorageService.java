945cb617cd6e9479c6e837991046a22c27ed4080
yingfeng-iu <yingfeng@iu.edu>
2019-04-18 17:23:16 -0400

    AMP-71: - added methods to compute directory and file path names for primary files and supplements at various levels. - renamed PrimaryController to FileUploadController - added logic to FileUploadController to upload primary file

--- src/main/java/edu/iu/dlib/amppd/service/FileStorageService.java
+++ src/main/java/edu/iu/dlib/amppd/service/FileStorageService.java
@@ -1,9 +1,12 @@
 package edu.iu.dlib.amppd.service;
 
-import java.nio.file.Path;
-
-import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.MultipartFile;
+
+import edu.iu.dlib.amppd.model.Collection;
+import edu.iu.dlib.amppd.model.Item;
+import edu.iu.dlib.amppd.model.Primary;
+import edu.iu.dlib.amppd.model.Supplement;
+import edu.iu.dlib.amppd.model.Unit;
 
 /**
  * Service for storing/retrieving files, including primary files, supplement files, as well as intermediate files.
@@ -12,7 +15,55 @@ import org.springframework.web.multipart.MultipartFile;
  */
 public interface FileStorageService {
 
+	/**
+	 * Stores the specified sourceFile to the specified targetPathname on the file system.
+	 * @param sourceFile the specified source File
+	 * @param targetPathname the specified target file path name relative to the storage root
+	 */
 	public void store(MultipartFile sourceFile, String targetPathname);
+	
+	/**
+	 * Returns the target storage directory path name relative to the storage root for the specified unit.
+	 * @param unit the specified unit
+	 * @return the target storage directory path name
+	 */
+	public String getDirPathName(Unit unit);
+
+	/**
+	 * Returns the target storage directory path name relative to the storage root for the specified collection.
+	 * @param collection the specified collection
+	 * @return the target storage directory path name
+	 */
+	public String getDirPathName(Collection collection);
+
+	/**
+	 * Returns the target storage directory path name relative to the storage root for the specified item.
+	 * @param item the specified item
+	 * @return the target storage directory path name
+	 */
+	public String getDirPathName(Item item);
+
+	/**
+	 * Returns the target storage directory path name relative to the storage root for the specified primary.
+	 * @param primary the specified primary
+	 * @return the target storage directory path name
+	 */
+	public String getDirPathName(Primary primary);
+
+	/**
+	 * Returns the target storage file path name relative to the storage root for the specified primary.
+	 * @param primary the specified primary
+	 * @return the target storage file path
+	 */
+	public String getFilePathName(Primary primary);
+
+	/**
+	 * Returns the target storage file path name relative to the storage root for the specified supplement.
+	 * @param supplement the specified supplement
+	 * @return the target storage file path
+	 */
+	public String getFilePathName(Supplement supplement);
+
 
 //	public Path load(String path);
 //
--- src/main/java/edu/iu/dlib/amppd/service/impl/FileStorageServiceImpl.java
+++ src/main/java/edu/iu/dlib/amppd/service/impl/FileStorageServiceImpl.java
@@ -1,23 +1,27 @@
 package edu.iu.dlib.amppd.service.impl;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
-import org.springframework.util.FileSystemUtils;
-import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import edu.iu.dlib.amppd.exception.StorageException;
-import edu.iu.dlib.amppd.exception.StorageFileNotFoundException;
+import edu.iu.dlib.amppd.model.Collection;
+import edu.iu.dlib.amppd.model.Item;
+import edu.iu.dlib.amppd.model.Primary;
+import edu.iu.dlib.amppd.model.Supplement;
+import edu.iu.dlib.amppd.model.SupplementOfCollection;
+import edu.iu.dlib.amppd.model.SupplementOfItem;
+import edu.iu.dlib.amppd.model.SupplementOfPrimary;
+import edu.iu.dlib.amppd.model.Unit;
 import edu.iu.dlib.amppd.service.FileStorageService;
 
 /**
@@ -33,11 +37,14 @@ import edu.iu.dlib.amppd.service.FileStorageService;
 public class FileStorageServiceImpl implements FileStorageService {
 
 	@Value("${amppd.filesys.root:/tmp/amppd/}")
+	private String rootPathName;
+
 	private Path root;
 
 	@Autowired
 	public FileStorageServiceImpl() {
 		try {
+			root = Paths.get(rootPathName);
 			Files.createDirectories(root);	// creates root directory if not already exists
 		}
 		catch (IOException e) {
@@ -45,6 +52,9 @@ public class FileStorageServiceImpl implements FileStorageService {
 		}
 	}
 
+	/**
+	 * @see edu.iu.dlib.amppd.service.FileStorageService.store(MultipartFile, String)
+	 */
 	public void store(MultipartFile file, String targetPathname) {
 //		String filename = StringUtils.cleanPath(file.getOriginalFilename());
 		try {
@@ -58,13 +68,82 @@ public class FileStorageServiceImpl implements FileStorageService {
 								+ targetPathname);
 			}
 			try (InputStream inputStream = file.getInputStream()) {
-				Files.copy(inputStream, this.root.resolve(targetPathname),
+				Files.copy(inputStream, root.resolve(targetPathname),
 						StandardCopyOption.REPLACE_EXISTING);
 			}
 		}
 		catch (IOException e) {
 			throw new StorageException("Failed to store file " + targetPathname, e);
 		}
+	}
+
+	/**
+	 * @see edu.iu.dlib.amppd.service.FileStorageService.getDirPathName(Unit)
+	 */
+	public String getDirPathName(Unit unit) {
+//		Path path = root.resolve("U-" + unit.getId());
+//		return path.toString();		
+		
+		// directory path for unit: U-<unitID>
+		return "U-" + unit.getId();		
+	}
+
+	/**
+	 * @see edu.iu.dlib.amppd.service.FileStorageService.getDirPathName(Collection)
+	 */
+	public String getDirPathName(Collection collection) {
+//		Path path = root.resolve("U-" + unit.getId());
+//		return path.toString();		
+		
+		// directory path for collection: U-<unitID/C-<collectionId>
+		return getDirPathName(collection.getUnit()) + File.pathSeparator + "C-" + collection.getId();
+	}
+
+	/**
+	 * @see edu.iu.dlib.amppd.service.FileStorageService.getDirPathName(Item)
+	 */
+	public String getDirPathName(Item item) {
+		// directory path for item: U-<unitID/C-<collectionId>/I-<itemId>
+		return getDirPathName(item.getCollection()) + File.pathSeparator + "I-" + item.getId();
+	}
+
+	/**
+	 * @see edu.iu.dlib.amppd.service.FileStorageService.getDirPathName(Primary)
+	 */
+	public String getDirPathName(Primary primary) {
+		// directory path for primary: U-<unitID/C-<collectionId>/I-<itemId>/P--<primaryId>
+		return getDirPathName(primary.getItem()) + File.pathSeparator + "P-" + primary.getId();
+	}
+	
+	/**
+	 * @see edu.iu.dlib.amppd.service.FileStorageService.getFilePathName(Primary)
+	 */
+	public String getFilePathName(Primary primary) {
+		// file path for primary: U-<unitID/C-<collectionId>/I-<itemId>/P--<primaryId>.<primaryExtension>
+		return getDirPathName(primary) + "."; // TODO add extension + primary.getExtension();
+	}
+
+	/**
+	 * @see edu.iu.dlib.amppd.service.FileStorageService.getFilePathName(Supplement)
+	 */
+	public String getFilePathName(Supplement supplement) {
+		// file name for supplement: S-<supplementId>
+		String fileName = "S" + supplement.getId() + "."; // TODO add extension + primary.getExtension();
+		
+		// directory path for supplement depends on the parent of the supplement, could be in the directory of collection/item/primary
+		String dirName = "";
+		
+		if (supplement instanceof SupplementOfCollection) {
+			dirName = getDirPathName(supplement.getCollection());
+		}
+		else if (supplement instanceof SupplementOfItem) {
+			dirName = getDirPathName(supplement.getItem());
+		}
+		else if (supplement instanceof SupplementOfPrimary) {
+			dirName = getDirPathName(supplement.getPrimary());
+		}
+		
+		return dirName + fileName;
 	}
 
 //	@Override
