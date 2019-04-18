55c5faed637280056c2a64e988afbcb61da1c63d
yingfeng-iu <yingfeng@iu.edu>
2019-04-18 17:23:16 -0400

    AMP-71: - added methods to compute directory and file path names for primary files and supplements at various levels. - renamed PrimaryController to FileUploadController - added logic to FileUploadController to upload primary file

---
+++ src/main/java/edu/iu/dlib/amppd/controller/FileUploadController.java
@@ -0,0 +1,70 @@
+package edu.iu.dlib.amppd.controller;
+
+import javax.websocket.server.PathParam;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import edu.iu.dlib.amppd.repository.PrimaryRepository;
+import edu.iu.dlib.amppd.service.FileStorageService;
+
+//TODO when we add controllers for data entities, we might want to move the actions into controllers for the associated entities.
+
+/**
+ * Controller to handle file upload for primaries and supplements.
+ * @author yingfeng
+ *
+ */
+@RestController
+public class FileUploadController {
+	
+    private final FileStorageService fileStorageService;
+    private final PrimaryRepository primaryRepository;
+//    private final SupplementRepository supplementRepository;
+
+    @Autowired
+    public FileUploadController(FileStorageService fileStorageService, PrimaryRepository primaryRepository) {
+        this.fileStorageService = fileStorageService;
+        this.primaryRepository = primaryRepository;
+    }
+
+    @PostMapping("/primary/{id}")
+    public String handlePrimaryUpload(@PathParam("id") Long primaryId, @RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) {
+        
+    	fileStorageService.store(file, );
+        redirectAttributes.addFlashAttribute("message",
+                "You successfully uploaded " + file.getOriginalFilename() + "!");
+
+        return "redirect:/";
+    }
+    
+//  @GetMapping("/")
+//  public String listUploadedFiles(Model model) throws IOException {
+//
+//      model.addAttribute("files", fileStorageService.loadAll().map(
+//              path -> MvcUriComponentsBuilder.fromMethodName(PrimaryController.class,
+//                      "serveFile", path.getFileName().toString()).build().toString())
+//              .collect(Collectors.toList()));
+//
+//      return "uploadForm";
+//  }
+
+//  @GetMapping("/files/{filename:.+}")
+//  @ResponseBody
+//  public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
+//
+//      Resource file = fileStorageService.loadAsResource(filename);
+//      return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
+//              "attachment; filename=\"" + file.getFilename() + "\"").body(file);
+//  }    
+
+//    @ExceptionHandler(StorageFileNotFoundException.class)
+//    public ResponseEntity<?> handleStorageFileNotFound(StorageFileNotFoundException exc) {
+//        return ResponseEntity.notFound().build();
+//    }
+
+}
