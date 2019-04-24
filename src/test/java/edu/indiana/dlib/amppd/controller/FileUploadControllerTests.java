3fd75817af8a91409cf3d2a4e09e1ccb9e11c8b7
yingfeng-iu <yingfeng@iu.edu>
2019-04-24 17:31:45 -0400

    AMP-69

--- src/test/java/edu/indiana/dlib/amppd/controller/FileUploadControllerTests.java
+++ src/test/java/edu/indiana/dlib/amppd/controller/FileUploadControllerTests.java
@@ -1,16 +1,15 @@
 package edu.indiana.dlib.amppd.controller;
 
-import java.nio.file.Paths;
-import java.util.stream.Stream;
+import static org.mockito.BDDMockito.then;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.fileUpload;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -23,17 +22,6 @@ import edu.indiana.dlib.amppd.model.Primaryfile;
 import edu.indiana.dlib.amppd.model.PrimaryfileSupplement;
 import edu.indiana.dlib.amppd.model.Unit;
 import edu.indiana.dlib.amppd.service.FileStorageService;
-
-import static org.mockito.BDDMockito.given;
-import static org.mockito.BDDMockito.then;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.fileUpload;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import hello.storage.StorageFileNotFoundException;
-import hello.storage.StorageService;
 
 @RunWith(SpringRunner.class)
 @AutoConfigureMockMvc
@@ -49,109 +37,105 @@ public class FileUploadControllerTests {
     @Test
     public void shouldSaveUploadedPrimaryfile() throws Exception {
     	Unit unit = new Unit();
-    	unit.setId(1);
+    	unit.setId(1l);
     	
     	Collection collection = new Collection();
-    	collection.setId(2);
+    	collection.setId(2l);
     	collection.setUnit(unit);
     	
     	Item item = new Item();
-    	item.setId(3);
+    	item.setId(3l);
     	item.setCollection(collection);
     	
     	Primaryfile primaryfile = new Primaryfile();
-    	primaryfile.setId(4);
+    	primaryfile.setId(4l);
     	primaryfile.setItem(item);
-    	
-    	String pathname = fileStorageService.getFilePathName(primaryfile); 	    
+    	primaryfile.setOriginalFileName("primaryfiletest.mp4");   
 
         MockMultipartFile multipartFile = new MockMultipartFile("file", "test.txt", "text/plain", "Test file upload".getBytes());
         this.mvc.perform(fileUpload("/primaryfile/4/file").file(multipartFile))
                 .andExpect(status().isFound())
-                .andExpect(header().string("Location", pathname));
+                .andExpect(header().string("Location", "U-1/C-2/I-3/P-4.mp4"));
 
-        then(fileStorageService).should().store(multipartFile, primaryfile);
+        then(fileStorageService).should().store(multipartFile, "U-1/C-2/I-3/P-4.mp4");
     }
 
     @Test
     public void shouldSaveUploadedCollectionSupplement() throws Exception {
     	Unit unit = new Unit();
-    	unit.setId(1);
+    	unit.setId(1l);
     	
     	Collection collection = new Collection();
-    	collection.setId(2);
+    	collection.setId(2l);
     	collection.setUnit(unit);
     	
     	CollectionSupplement supplement = new CollectionSupplement();
-    	supplement.setId(3);
-    	supplement.setCollection(collection);
+    	supplement.setId(3l);
+    	supplement.setCollection(collection);  
+    	supplement.setOriginalFileName("supplementtest.pdf");
     	
-    	String pathname = fileStorageService.getFilePathName(supplement); 	    
-
         MockMultipartFile multipartFile = new MockMultipartFile("file", "test.txt", "text/plain", "Test file upload".getBytes());
         this.mvc.perform(fileUpload("/supplement/3/file").file(multipartFile))
                 .andExpect(status().isFound())
-                .andExpect(header().string("Location", pathname));
+                .andExpect(header().string("Location", "U-1/C-2/S-3.pdf"));
 
-        then(fileStorageService).should().store(multipartFile, supplement);
+        then(fileStorageService).should().store(multipartFile, "U-1/C-2/S-3.pdf");
     }
 
     @Test
     public void shouldSaveUploadedItemSupplement() throws Exception {
     	Unit unit = new Unit();
-    	unit.setId(1);
+    	unit.setId(1l);
     	
     	Collection collection = new Collection();
-    	collection.setId(2);
+    	collection.setId(2l);
     	collection.setUnit(unit);
     	
     	Item item = new Item();
-    	item.setId(3);
+    	item.setId(3l);
     	item.setCollection(collection);
     	
     	ItemSupplement supplement = new ItemSupplement();
-    	supplement.setId(4);
+    	supplement.setId(4l);
     	supplement.setItem(item);
-    	
-    	String pathname = fileStorageService.getFilePathName(supplement); 	    
+    	supplement.setOriginalFileName("supplementtest.pdf");    
 
         MockMultipartFile multipartFile = new MockMultipartFile("file", "test.txt", "text/plain", "Test file upload".getBytes());
         this.mvc.perform(fileUpload("/supplement/4/file").file(multipartFile))
                 .andExpect(status().isFound())
-                .andExpect(header().string("Location", pathname));
+                .andExpect(header().string("Location", "U-1/C-2/I-3/S-4.pdf"));
 
-        then(fileStorageService).should().store(multipartFile, supplement);
+        then(fileStorageService).should().store(multipartFile, "U-1/C-2/I-3/S-4.pdf");
     }
 
     @Test
     public void shouldSaveUploadedPrimaryfileSupplement() throws Exception {
     	Unit unit = new Unit();
-    	unit.setId(1);
+    	unit.setId(1l);
     	
     	Collection collection = new Collection();
-    	collection.setId(2);
+    	collection.setId(2l);
     	collection.setUnit(unit);
     	
     	Item item = new Item();
-    	item.setId(3);
+    	item.setId(3l);
     	item.setCollection(collection);
     	
     	Primaryfile primaryfile = new Primaryfile();
-    	primaryfile.setId(4);
+    	primaryfile.setId(4l);
     	primaryfile.setItem(item);
     	
     	PrimaryfileSupplement supplement = new PrimaryfileSupplement();
-    	supplement.setId(5);
+    	supplement.setId(5l);
     	supplement.setPrimaryfile(primaryfile);
-    	
-    	String pathname = fileStorageService.getFilePathName(supplement); 	    
+    	supplement.setOriginalFileName("supplementtest.pdf");
 
         MockMultipartFile multipartFile = new MockMultipartFile("file", "test.txt", "text/plain", "Test file upload".getBytes());
         this.mvc.perform(fileUpload("/supplement/5/file").file(multipartFile))
                 .andExpect(status().isFound())
-                .andExpect(header().string("Location", pathname));
+                .andExpect(header().string("Location", "U-1/C-2/I-3/P-4/S-5.pdf"));
 
-        then(fileStorageService).should().store(multipartFile, supplement);
+        then(fileStorageService).should().store(multipartFile, "U-1/C-2/I-3/P-4/S-5.pdf");
     }
 
 //    @SuppressWarnings("unchecked")
