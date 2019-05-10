771078510d531b8abd083cc870132ac1028fbe3f
vinitab <vinitab@bl-libg-ltdmsde.ads.iu.edu>
2019-05-10 12:22:26 -0400

    AMP-113_tests

--- src/main/java/edu/indiana/dlib/amppd/repository/ItemRepository.java
+++ src/main/java/edu/indiana/dlib/amppd/repository/ItemRepository.java
@@ -7,4 +7,12 @@ import edu.indiana.dlib.amppd.model.Item;
 @RepositoryRestResource(collectionResourceRel = "items", path = "items")
 public interface ItemRepository extends ContentRepository<Item> {
 	
+	List<Item> findByDescription(@Param("description") String description);
+	
+	List<Item> findByCreatedDate(@Param("createdDate") String createdDate);
+	
+	List<Item> findByCreatedBy(@Param("createdBy") String createdBy);
+	
+	
+	
 }
--- src/main/java/edu/indiana/dlib/amppd/repository/PrimaryfileRepository.java
+++ src/main/java/edu/indiana/dlib/amppd/repository/PrimaryfileRepository.java
@@ -2,9 +2,16 @@ package edu.indiana.dlib.amppd.repository;
 
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import edu.indiana.dlib.amppd.model.Item;
 import edu.indiana.dlib.amppd.model.Primaryfile;
 
 @RepositoryRestResource(collectionResourceRel = "primaryfiles", path = "primaryfiles")
 public interface PrimaryfileRepository extends AssetRepository<Primaryfile> {
+	
+	List<Primaryfile> findByDescription(@Param("description") String description);
+	
+	List<Primaryfile> findByCreatedDate(@Param("createdDate") String createdDate);
+	
+	List<Primaryfile> findByCreatedBy(@Param("createdBy") String createdBy);
 	
 }
