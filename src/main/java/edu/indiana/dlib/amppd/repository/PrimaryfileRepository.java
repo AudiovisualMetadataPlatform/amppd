f982de3ac0c864875cd3d4037f03d322bd063d32
Vinita Boolchandani <vinitab@iu.edu>
2019-05-13 16:59:10 -0400

    Amp 113 factories (#18)
    * AMP-113_factories
    
    * AMP-113_factories
    
    * AMP-113_factories
    
    * AMP-113_factories
    
    * AMP-113_factories
    
    * AMP-113_tests
    
    * AMP-113_tests
    
    * AMP-113_packagePath_UnusedImports.
    
    * AMP-113_CodeReviewChanges
    
    * AMP-113_factories
    
    * AMP-113_factories
    
    * AMP-113_rebase
    
    * AMP-113_factories
    
    * AMP-113_factories
    
    * AMP-113_tests
    
    * AMP-113_tests
    
    * AMP-113_packagePath_UnusedImports.
    
    * AMP-113_CodeReviewChanges
    
    * AMP-113_codeReviewFixes
    
    * AMP-113_removingComments
    
    * AMP-113_FixingExceptions
    
    * AMP-113_finalChanges
    
    * AMP-113_CircleCIFixes

--- src/main/java/edu/indiana/dlib/amppd/repository/PrimaryfileRepository.java
+++ src/main/java/edu/indiana/dlib/amppd/repository/PrimaryfileRepository.java
@@ -2,9 +2,11 @@ package edu.indiana.dlib.amppd.repository;
 
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import edu.indiana.dlib.amppd.model.Item;
 import edu.indiana.dlib.amppd.model.Primaryfile;
 
 @RepositoryRestResource(collectionResourceRel = "primaryfiles", path = "primaryfiles")
 public interface PrimaryfileRepository extends AssetRepository<Primaryfile> {
+	
 	
 }
--- src/test/java/edu/indiana/dlib/amppd/repository/ItemRepositoryTests.java
+++ src/test/java/edu/indiana/dlib/amppd/repository/ItemRepositoryTests.java
@@ -11,6 +11,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.HashMap;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -20,7 +22,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import edu.indiana.dlib.amppd.model.factory.ObjectFactory;
+import edu.indiana.dlib.amppd.model.Item;
 import edu.indiana.dlib.amppd.repository.ItemRepository;
 
 @RunWith(SpringRunner.class)
@@ -32,7 +39,20 @@ public class ItemRepositoryTests {
 
 	@Autowired
 	private ItemRepository itemRepository;
-
+	
+	@Autowired 
+	private ObjectMapper mapper = new ObjectMapper();
+	private Item objItem ;
+	private ObjectFactory objFactory = new ObjectFactory();
+	
+	@Before
+	public void initiateBeforeTests() 
+	{
+		HashMap params = new HashMap<String, String>();
+		objItem= (Item)objFactory.createDataentityObject(params, "Item");
+		
+	}
+	
 	@Before
 	public void deleteAllBeforeTests() throws Exception {
 		itemRepository.deleteAll();
@@ -70,9 +90,10 @@ public class ItemRepositoryTests {
 	@Test
 	public void shouldQueryItem() throws Exception {
 
-		mockMvc.perform(post("/items").content(
-				"{ \"name\": \"Item 1\", \"description\":\"For test\"}")).andExpect(
-						status().isCreated());
+		  mockMvc.perform(post("/items").content(
+		  "{ \"name\": \"Item 1\", \"description\":\"For test\"}")).andExpect(
+		  status().isCreated());
+		 
 
 		mockMvc.perform(
 				get("/items/search/findByName?name={name}", "Item 1")).andExpect(
@@ -80,7 +101,60 @@ public class ItemRepositoryTests {
 								jsonPath("$._embedded.items[0].name").value(
 										"Item 1"));
 	}
+	
+	
+	@Test
+	public void shouldQueryItemDescription() throws Exception {
+		
+		objItem.setName("Item 200");
+		objItem.setDescription("For testing Item Respository using Factories");
+		String json = mapper.writeValueAsString(objItem);
+		mockMvc.perform(post("/items")
+				  .content(json)).andExpect(
+						  status().isCreated());
+		mockMvc.perform(
+				get("/items/search/findByDescription?description={description}", "For testing Item Respository using Factories")).andDo(
+						MockMvcResultHandlers.print()).andExpect(
+						status().isOk()).andExpect(
+								jsonPath("$._embedded.items[0].description").value(
+										"For testing Item Respository using Factories"));
+	}
 
+	
+	@Test
+	public void shouldQueryItemCreatedBy() throws Exception {
+		
+		objItem.setName("Item 200");
+		objItem.setDescription("For testing Item Respository using Factories");
+		objItem.setCreatedBy("Developer");
+		String json = mapper.writeValueAsString(objItem);
+		mockMvc.perform(post("/items")
+				  .content(json)).andExpect(
+						  status().isCreated());
+		mockMvc.perform(
+				get("/items/search/findByCreatedBy?createdBy={createdBy}", "Developer")).andDo(
+						MockMvcResultHandlers.print()).andExpect(
+						status().isOk()).andExpect(
+								jsonPath("$._embedded.items[0].createdBy").value(
+										"Developer"));
+	}
+	
+	/*
+	 * @Test public void shouldQueryItemCreatedDate() throws Exception {
+	 * 
+	 * objItem.setName("Item 200");
+	 * objItem.setDescription("For testing Item Respository using Factories");
+	 * objItem.setCreatedDate(1012019); String json =
+	 * mapper.writeValueAsString(objItem); mockMvc.perform(post("/items")
+	 * .content(json)).andExpect( status().isCreated()); mockMvc.perform(
+	 * get("/items/search/findByCreatedDate?createdDate={createdDate}",
+	 * "1012019")).andDo( MockMvcResultHandlers.print()).andExpect(
+	 * status().isOk()).andExpect(
+	 * jsonPath("$._embedded.items[0].createdDate").value( "1012019")); }
+	 */
+
+	
+	
 	@Test
 	public void shouldUpdateItem() throws Exception {
 
