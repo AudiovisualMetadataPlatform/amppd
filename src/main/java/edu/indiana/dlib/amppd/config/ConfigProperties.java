cd48c4b3e4f2fa43aa837b3e555d54ed09ebdb8e
Adeel Ahmad <adeelahmad99@gmail.com>
2019-07-25 18:07:12 +0500

    Adding blend4j & importing dir from fs

---
+++ src/main/java/edu/indiana/dlib/amppd/config/ConfigProperties.java
@@ -0,0 +1,26 @@
+package edu.indiana.dlib.amppd.config;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import javax.validation.constraints.NotNull;
+
+
+/**
+ * Configuration for persistence layer.
+ * @author adeelahmad
+ *
+ */
+@ConfigurationProperties(prefix="amppd")
+@Getter
+@Setter
+public class ConfigProperties {
+
+
+    @NotNull private String galaxyhost;
+    @NotNull private String galaxyport;
+    @NotNull private String galaxykey;
+
+    //getters and setters
+}
