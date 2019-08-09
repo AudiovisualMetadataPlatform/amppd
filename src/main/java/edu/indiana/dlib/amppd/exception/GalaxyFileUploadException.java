ff42d57cf45a832244fe42fabd92ccd7ecba34b7
yingfeng-iu <yingfeng@iu.edu>
2019-08-09 10:08:03 -0400

    AMP-276: add more logging and exception handling

---
+++ src/main/java/edu/indiana/dlib/amppd/exception/GalaxyFileUploadException.java
@@ -0,0 +1,12 @@
+package edu.indiana.dlib.amppd.exception;
+
+public class GalaxyFileUploadException  extends StorageException {
+
+    public GalaxyFileUploadException(String message) {
+        super(message);
+    }
+
+    public GalaxyFileUploadException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
