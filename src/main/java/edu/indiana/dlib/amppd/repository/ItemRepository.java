f9e1b39fd10b5dc45315f49e802389e3987e9245
Ying Feng <yingfeng-iu@users.noreply.github.com>
2019-11-01 10:36:00 -0400

    AMP-443: add logic and tests for search item by name/description (#49)

--- src/main/java/edu/indiana/dlib/amppd/repository/ItemRepository.java
+++ src/main/java/edu/indiana/dlib/amppd/repository/ItemRepository.java
@@ -1,5 +1,9 @@
 package edu.indiana.dlib.amppd.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import edu.indiana.dlib.amppd.model.Item;
@@ -7,4 +11,7 @@ import edu.indiana.dlib.amppd.model.Item;
 @RepositoryRestResource(collectionResourceRel = "items", path = "items")
 public interface ItemRepository extends ContentRepository<Item> {
 	
+	@Query("select i from Item i where i.name like %:keyword% or i.description like %:keyword%")
+	List<Item> findByKeyword(@Param("keyword") String keyword); 
+
 }
