f3851116b2692d7b074bca9b74d63c0050d7e90a
yingfeng-iu <yingfeng@iu.edu>
2019-11-12 15:37:35 -0500

    AMP-467: added @CrossOrigin for all repositories and controllers

--- src/main/java/edu/indiana/dlib/amppd/controller/BundleController.java
+++ src/main/java/edu/indiana/dlib/amppd/controller/BundleController.java
@@ -1,6 +1,7 @@
 package edu.indiana.dlib.amppd.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,6 +16,7 @@ import lombok.extern.java.Log;
  * @author yingfeng
  *
  */
+@CrossOrigin(origins = "*")
 @RestController
 @Log
 public class BundleController {
--- src/main/java/edu/indiana/dlib/amppd/repository/AmpUserRepository.java
+++ src/main/java/edu/indiana/dlib/amppd/repository/AmpUserRepository.java
@@ -1,9 +1,11 @@
 package edu.indiana.dlib.amppd.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import edu.indiana.dlib.amppd.model.AmpUser;
 
+@CrossOrigin(origins = "*")
 public class AmpUserRepository {
 	
 	public interface UserRepository extends JpaRepository<AmpUser, Long> {
