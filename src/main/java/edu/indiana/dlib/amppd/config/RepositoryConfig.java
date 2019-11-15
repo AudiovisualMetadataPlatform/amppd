d83b7f2f179d71bb91c88b0ff135cd448499ff54
Ying Feng <yingfeng-iu@users.noreply.github.com>
2019-11-15 12:20:40 -0500

    AMP-483: add repository config to expose all IDs (#53)

---
+++ src/main/java/edu/indiana/dlib/amppd/config/RepositoryConfig.java
@@ -0,0 +1,24 @@
+package edu.indiana.dlib.amppd.config;
+
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.metamodel.EntityType;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+
+@Configuration
+public class RepositoryConfig implements RepositoryRestConfigurer {
+    @PersistenceContext
+    private EntityManager entityManager;
+	
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+    	config.exposeIdsFor(
+                entityManager.getMetamodel().getEntities().stream()
+                .map(EntityType::getJavaType)
+                .toArray(Class[]::new));
+    }
+}
