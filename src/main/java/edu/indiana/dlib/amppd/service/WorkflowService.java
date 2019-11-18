e00feb783c31d9fdb2f3958eb958b5d4b211dcbb
yingfeng-iu <yingfeng@iu.edu>
2019-11-18 16:50:03 -0500

    AMP-345_validateWorkflow

--- src/main/java/edu/indiana/dlib/amppd/service/WorkflowService.java
+++ src/main/java/edu/indiana/dlib/amppd/service/WorkflowService.java
@@ -1,7 +1,12 @@
 package edu.indiana.dlib.amppd.service;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
 import com.github.jmchilton.blend4j.galaxy.WorkflowsClient;
 import com.github.jmchilton.blend4j.galaxy.beans.Workflow;
+import com.github.jmchilton.blend4j.galaxy.beans.WorkflowDetails;
 
 /**
  * Service for workflow related functionalities.
@@ -16,10 +21,32 @@ public interface WorkflowService {
 	public WorkflowsClient getWorkflowsClient();
 	
 	/**
+	 * Validate the specified workflow and return errors if any.
+	 * @param workflowDetails the details of the specified workflow 
+	 * @return a list of validation errors for invalid workflow, otherwise empty list 
+	 */
+	public List<String> validateWorkflow(WorkflowDetails workflowDetails);
+	
+	/**
+	 * Validate the specified workflow and return errors if any.
+	 * @param workflow the ID of the specified workflow 
+	 * @return a list of validation errors for invalid workflow, otherwise empty list 
+	 */
+	public List<String> validateWorkflow(String workflowId);
+
+	/**
+	 * List all workflows currently existing in Galaxy, including/excluding invalid ones based on the specification.
+	 * @param excludeInvalid whether to exclude the invalid workflows
+	 * @return a list of inquired workflows with name, ID, and URL.
+	 */
+	@GetMapping("/workflows")
+	public List<Workflow> listWorkflows(Boolean excludeInvalid);
+	
+	/**
 	 * Return the workflow with the given name.
 	 * @param workflowName the name of the specified workflow
 	 * @return the workflow requested
 	 */
 	public Workflow getWorkflow(String workflowName);
-	
+		
 }
--- src/main/java/edu/indiana/dlib/amppd/service/impl/WorkflowServiceImpl.java
+++ src/main/java/edu/indiana/dlib/amppd/service/impl/WorkflowServiceImpl.java
@@ -1,5 +1,8 @@
 package edu.indiana.dlib.amppd.service.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +10,9 @@ import org.springframework.stereotype.Service;
 
 import com.github.jmchilton.blend4j.galaxy.WorkflowsClient;
 import com.github.jmchilton.blend4j.galaxy.beans.Workflow;
+import com.github.jmchilton.blend4j.galaxy.beans.WorkflowDetails;
 
+import edu.indiana.dlib.amppd.exception.GalaxyWorkflowException;
 import edu.indiana.dlib.amppd.service.GalaxyApiService;
 import edu.indiana.dlib.amppd.service.WorkflowService;
 import lombok.Getter;
@@ -35,6 +40,44 @@ public class WorkflowServiceImpl implements WorkflowService {
 	public void init() {
 		workflowsClient = galaxyApiService.getGalaxyInstance().getWorkflowsClient();
 	}	
+	
+	/**
+	 * @see edu.indiana.dlib.amppd.service.WorkflowService.isValidWorkflow(WorkflowDetails)
+	 */	
+	@Override	
+	public List<String> validateWorkflow(WorkflowDetails workflowDetails) {
+		List<String> errors = new ArrayList<String>();
+
+		if (workflowDetails == null) {
+			errors.add("The provided workflow is null.");
+		}
+		else if (workflowDetails.getInputs() == null) {
+			errors.add("The provided workflow has null inputs.");
+		}
+		else if (workflowDetails.getInputs().keySet().size() == 0) {
+			errors.add("The provided workflow has no inputs.");
+		}
+		// for AMPPD we require that a workflow only takes one primaryfile as input
+		else if (workflowDetails.getInputs().keySet().size() > 1) {
+			errors.add("The provided workflow has no inputs.");
+		}
+		// At this point, we only check workflow input; in the future we might add more validation rules.
+
+		return errors;
+	}
+	
+	/**
+	 * @see edu.indiana.dlib.amppd.service.WorkflowService.isValidWorkflow(String)
+	 */	
+	@Override	
+	public  List<String> isValidWorkflow(String workflowId) {
+		try {
+			return isValidWorkflow(workflowsClient.showWorkflow(workflowId));
+		}
+		catch (Exception e) {
+			throw new GalaxyWorkflowException("Exception when retrieving details for workflow " + workflowId);
+		}
+	}
 	
 	/**
 	 * @see edu.indiana.dlib.amppd.service.WorkflowService.getWorkflow(String)
