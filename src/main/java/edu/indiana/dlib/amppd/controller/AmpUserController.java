8273e364965c509c664db52bff2f86872f9496b7
Vinita Boolchandani <vinitab@iu.edu>
2019-11-19 15:57:34 -0500

    AMP-439_branch2FinalCommit (#56)
    * AMP-439_branch2FinalCommit
    
    * AMP-439_addingInterface
    
    * AMP-439_fixingApproved

--- src/main/java/edu/indiana/dlib/amppd/controller/AmpUserController.java
+++ src/main/java/edu/indiana/dlib/amppd/controller/AmpUserController.java
@@ -1,14 +1,15 @@
-
-  package edu.indiana.dlib.amppd.controller;
+package edu.indiana.dlib.amppd.controller;
   
-  import org.springframework.ui.Model;
+  import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import edu.indiana.dlib.amppd.repository.AmpUserRepository;
+import edu.indiana.dlib.amppd.model.AmpUser;
+import edu.indiana.dlib.amppd.service.impl.AmpUserServiceImpl;
 import lombok.extern.java.Log;
   
  /**
@@ -21,20 +22,43 @@ import lombok.extern.java.Log;
   @RestController
   @Log 
   public class AmpUserController{
+	  @Autowired
+	  private AmpUserServiceImpl ampService;
+	  private static AmpUser user;
+	  
+	  @GetMapping("/amp/auth") 
+	  public @ResponseBody boolean loginAuth(
+			@RequestParam("name") String name,
+			@RequestParam("pswd") String pswd){ 
+		boolean res = false;
+		log.info("Login Authenticaton for User=> Name:"+ name);	
+		res = ampService.validate(name, pswd);
+		log.info(" Authenticaton result:"+res);
+		return res;
+	  }
+	  
+	  @GetMapping("/amp/register") 
+	  public @ResponseBody boolean register(
+			@RequestParam("name") String name,
+			@RequestParam("pswd") String pswd){ 
+		boolean res = false;
+		log.info("Registeration for User=> Name:"+ name);	
+		user = new AmpUser();
+		user.setUsername(name); 
+		user.setPassword(pswd);
+		//user.setApproved(false);
+		res = ampService.registerAmpUser(user);
+		log.info(" Registeration result:"+res);
+		return res;
+	  }
+	  
   
-	private AmpUserRepository ampUser;
-	
-	
-	  @GetMapping("/login") public String index() { return "index"; }
-	  
-	  @PostMapping("/login") String login(@RequestParam(value="name") String name)
-	  { return "redirect:/home"; }
-	  
-	  @PostMapping("/register") String register(@RequestParam(value="name") String
-	  name) { return "redirect:/home"; }
-	  
-	  @GetMapping("/welcome") public String welcome(Model model) { return "home"; }
-	 
+	/*
+	 * @PostMapping("/register") String register(@RequestParam(value="name") String
+	 * name) { return "redirect:/home"; }
+	 * 
+	 * @GetMapping("/welcome") public String welcome(Model model) { return "home"; }
+	 */
 	 
   }
 		 
