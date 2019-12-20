56bb2562c46dca1284131b39fef258ac5aac0c1a
Dan Fischer <31411749+dfische3@users.noreply.github.com>
2019-12-20 15:54:26 -0600

    Amp 498 validating credentials updated (#58)
    * Changing AmpUserController to accept posts instead of get requests.
    
    * Updating tests
    
    * Adding .metadata to gitignore
    
    * Removing .metadata from fommit
    
    * Removing DS_Store

--- src/main/java/edu/indiana/dlib/amppd/repository/AmpUserRepository.java
+++ src/main/java/edu/indiana/dlib/amppd/repository/AmpUserRepository.java
@@ -1,5 +1,7 @@
 package edu.indiana.dlib.amppd.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -12,6 +14,13 @@ import edu.indiana.dlib.amppd.model.AmpUser;
 @RepositoryRestResource(collectionResourceRel = "users", path = "users")
 public interface AmpUserRepository extends CrudRepository<AmpUser, Long>{
 	
-	@Query(value = "select 1 from AmpUser i where i.username = :username and i.password = :pswd")
-	String findByUsername(@Param("username") String username, @Param("pswd") String pswd);
+	@Query(value = "select 1 from AmpUser i where i.username = :username and i.password = :pswd and i.approved=true")
+	String findByApprovedUsername(@Param("username") String username, @Param("pswd") String pswd);
+	
+	@Query(value = "select i from AmpUser i where i.username = :username")
+	List<AmpUser> findByUsername(@Param("username") String username);
+	
+	@Query(value = "select case when COUNT(*)>0 then true else false end from AmpUser i where i.username = :username")
+	boolean usernameExists(@Param("username") String username);
+	
 }
