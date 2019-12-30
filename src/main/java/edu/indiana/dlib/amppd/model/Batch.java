e2611efda8aa3a679dc8be59bde62bd7cc69c484
yingfeng-iu <yingfeng@iu.edu>
2019-12-30 12:55:02 -0500

    AMP-525: add DB mapping

--- src/main/java/edu/indiana/dlib/amppd/model/Batch.java
+++ src/main/java/edu/indiana/dlib/amppd/model/Batch.java
@@ -3,17 +3,30 @@ package edu.indiana.dlib.amppd.model;
 import java.util.Date;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.OneToMany;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.Data;
+
 /**
  * Class containing information of a batch manifest. Fields that apply to the whole spreadsheet such as collection name are entered via UI; 
  * while fields of each batch file comes from each row in the manifest spreadsheet.
  * @author yingfeng
  *
  */
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Data
 public class Batch {
 	AmpUser submitUser;
 	Date submitTime;
 	String manifestFilename;
 	Collection collection;
 	
+	@OneToMany(mappedBy="batch")
 	List<BatchFile> batchFiles;
+	
 }
--- src/main/java/edu/indiana/dlib/amppd/model/BatchFile.java
+++ src/main/java/edu/indiana/dlib/amppd/model/BatchFile.java
@@ -1,10 +1,21 @@
 package edu.indiana.dlib.amppd.model;
 
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.ManyToOne;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.Data;
+
 /**
  * Class containing information of a batch file. This corresponds to a row in a batch manifest spreadsheet.
  * @author yingfeng
  *
  */
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Data
 public class BatchFile {	
 	public enum FileType { PRIMARYFILE, COLLECTION_SUPPLEMENT, ITEM_SUPPLEMENT, PRIMARYFILE_SUPPLEMENT };
 
@@ -17,4 +28,8 @@ public class BatchFile {
 	String primaryfileName;
 	String primaryfileDescription;
 	String supplementFilename;	
+	
+	@ManyToOne
+	Batch batch;
+	
 }
