0867e7cde47751e3a08c1948c667f93b83e86ebd
yingfeng-iu <yingfeng@iu.edu>
2020-01-21 17:41:20 -0500

    AMP-520: add data model for HMGM

--- src/main/java/edu/indiana/dlib/amppd/model/AmpUser.java
+++ src/main/java/edu/indiana/dlib/amppd/model/AmpUser.java
@@ -2,11 +2,17 @@ package edu.indiana.dlib.amppd.model;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+
+import java.util.Set;
+
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
 import lombok.ToString;
 
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -19,7 +25,13 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 @ToString(callSuper=true, onlyExplicitlyIncluded=true)
 public class AmpUser {
 	
-    public enum Role {USER, ADMIN, USER_MANAGER}
+	/* Note:
+	 * In the future when we implement full-featured access control, we shall define separate classes for Role and Permission, 
+	 * and use User-Role-Permission relations to control access. 
+	 * For now we use a simplified strategy with a flat role definition, i.e. each user has a static Role associated with specific permissions.
+	 * Supervisor and Student are for Human MGM use only, and we assume they don't overlap with AMPPD Content User such as Collection Manager.
+	 */	
+    public enum Role {ADMIN, MANAGER, SUPERVISOR, STUDENT}
 	
 	@Id
     @GeneratedValue(strategy=GenerationType.AUTO)
@@ -31,4 +43,7 @@ public class AmpUser {
 	
 	// TODO change to Enum state (RQUESTED, APPROVED, ACTIVATED,  REJECTED) 
 	private Boolean approved = false;	
+	
+	@OneToMany(mappedBy="assignedTo")
+    private Set<Hmgmtask> hmgmtasks;
 }
---
+++ src/main/java/edu/indiana/dlib/amppd/model/Hmgmtask.java
@@ -0,0 +1,61 @@
+package edu.indiana.dlib.amppd.model;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.ManyToOne;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * Class representing a Human MGM task, which is performed by an HMGM user, on a particular output from a particular AMPPD job step 
+ * (i.e. Galaxy workflow step), for a particular purpose. The status of a task goes through a finite set of changes as it's being worked on.
+ * @author yingfeng
+ *
+ */
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Data
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper=true)
+@ToString(callSuper=true)
+public class Hmgmtask extends Dataentity {
+	
+	public enum Type {TRANSCRIPT, NER, STRUCTURE};
+	public enum Status {OPEN, READY, ASSIGNED, PROGRESS, REVIEW, COMPLETED};
+	public enum Level {PASS, AVERAGE, DETAILED};
+
+	// The following Galaxy ID fields uniquely identify the Galaxy dataset (an output file from a particular job)  to be worked on for this task	
+	private String workflowId;
+	private String invocationId;
+	private String stepId;
+	private String datasetId;
+	
+	// purpose of the task, could be: Transcript correction, NER output revision, Structural metadata creation
+	private Type type;
+
+	// degree of refinement required of the result, could be: pass-through, average, detailed
+	private Level level;
+	
+	// current status, could be: Open, Ready (level of detail has been provided by supervisor), Assigned, In progress, Under review, Completed
+	private Status status;
+
+	// the path (relative to amppd root) for the temporary file saved while work is in progress
+	private String tmpFilepath;
+	
+//	// ID of the user currently assigned to the task
+//	private Long assignedUserId;
+	
+	@ManyToOne 
+	private AmpUser assignedTo;
+
+	/* Note: 
+	 * In the future we could add a list of TaskAction to record the history of a task workflow; 
+	 * however, for now we can acquire the history via DB auditing tools which is sufficient for internal use.
+	 */
+	
+}
