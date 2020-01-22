b48e9c0e7d67bf6f940256a9a27d40e92eaaa8f4
Ying Feng <yingfeng-iu@users.noreply.github.com>
2020-01-22 12:41:59 -0500

    AMP-520: add data model for HMGM (#69)
    * AMP-520: add data model for HMGM
    
    * AMP-520/AMP-524:
    - added repo for HMGM task
    - for AMP-524: added extra test for long description as i noticed that
    the DB update does not take effect without re-creating the tables.
    
    * AMP-524: commented code remvoed
    
    * AMP-520: adding Note for HMGM task
    
    * AMP-520: some clean up
    
    * AMP-520

---
+++ src/main/java/edu/indiana/dlib/amppd/model/HmgmNote.java
@@ -0,0 +1,44 @@
+package edu.indiana.dlib.amppd.model;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Class representing a Human MGM task note, which is added by a task assignee or a supervisor for communication.
+ * @author yingfeng
+ *
+ */
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Data
+@NoArgsConstructor
+public class HmgmNote {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+
+    @CreatedDate
+    private long createdDate;
+    
+    @CreatedBy
+    private String createdBy;
+
+    // the actual content of the note
+    private String text;
+    
+	@ManyToOne 
+	private HmgmTask hmgmTask;
+
+}
