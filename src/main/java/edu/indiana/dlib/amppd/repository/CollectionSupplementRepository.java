8b854b5d12967f2b4a9c0c86ef99893207c3dbde
dan <dan@portalmedia.com>
2020-01-22 09:18:31 -0600

    Removing unused references.  Removing DS_Store file and adding it to .gitignore

--- src/main/java/edu/indiana/dlib/amppd/repository/CollectionSupplementRepository.java
+++ src/main/java/edu/indiana/dlib/amppd/repository/CollectionSupplementRepository.java
@@ -1,9 +1,5 @@
 package edu.indiana.dlib.amppd.repository;
 
-import java.util.List;
-
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
--- src/main/java/edu/indiana/dlib/amppd/repository/PrimaryfileSupplementRepository.java
+++ src/main/java/edu/indiana/dlib/amppd/repository/PrimaryfileSupplementRepository.java
@@ -1,13 +1,8 @@
 package edu.indiana.dlib.amppd.repository;
 
-import java.util.List;
-
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-import edu.indiana.dlib.amppd.model.Primaryfile;
 import edu.indiana.dlib.amppd.model.PrimaryfileSupplement;
 
 @CrossOrigin(origins = "*")
