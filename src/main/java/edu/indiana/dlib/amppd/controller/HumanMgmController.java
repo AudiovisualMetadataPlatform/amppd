a542a94c86edef5a996cf2f6c2043d3f8da3c7b9
dan <dan@portalmedia.com>
2020-02-07 08:13:31 -0600

    Adding bbc transcript editor.  Adding google json parser to project.

--- src/main/java/edu/indiana/dlib/amppd/controller/HumanMgmController.java
+++ src/main/java/edu/indiana/dlib/amppd/controller/HumanMgmController.java
@@ -1,35 +1,30 @@
 package edu.indiana.dlib.amppd.controller;
 
-import java.util.Date;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
-import java.util.Set;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
-import com.github.jmchilton.blend4j.galaxy.beans.Dataset;
-import com.github.jmchilton.blend4j.galaxy.beans.Invocation;
-import com.github.jmchilton.blend4j.galaxy.beans.InvocationDetails;
-import com.github.jmchilton.blend4j.galaxy.beans.JobInputOutput;
-import com.github.jmchilton.blend4j.galaxy.beans.WorkflowOutputs;
+import com.github.jmchilton.blend4j.galaxy.GalaxyInstance;
+import com.github.jmchilton.blend4j.galaxy.HistoriesClient;
+import com.github.jmchilton.blend4j.galaxy.LibrariesClient;
+import com.github.jmchilton.blend4j.galaxy.SearchClient.SearchResponse;
+import com.github.jmchilton.blend4j.galaxy.beans.History;
+import com.github.jmchilton.blend4j.galaxy.beans.HistoryContents;
+import com.github.jmchilton.blend4j.galaxy.beans.Library;
+import com.github.jmchilton.blend4j.galaxy.beans.LibraryContent;
 
-import edu.indiana.dlib.amppd.model.AmpUser;
-import edu.indiana.dlib.amppd.model.Unit;
-import edu.indiana.dlib.amppd.repository.UnitRepository;
-import edu.indiana.dlib.amppd.service.AmpUserService;
-import edu.indiana.dlib.amppd.service.BatchService;
-import edu.indiana.dlib.amppd.service.BatchValidationService;
-import edu.indiana.dlib.amppd.service.JobService;
-import edu.indiana.dlib.amppd.web.BatchValidationResponse;
-import edu.indiana.dlib.amppd.web.HmgmResourceResponse;
+import edu.indiana.dlib.amppd.service.GalaxyApiService;
+import lombok.Getter;
 import lombok.extern.java.Log;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
@@ -38,37 +33,65 @@ import lombok.extern.java.Log;
 public class HumanMgmController {
 	
 
+	public static final String SHARED_LIBARY_NAME = "Amppd Library";
+	public static final String SHARED_HISTORY_NAME = "Amppd History";
+	
 	@Autowired
-	private JobService jobService;   
+	private GalaxyApiService galaxyApiService;
+	
+	private GalaxyInstance galaxyInstance;
+	
+	@Getter
+	private LibrariesClient librariesClient;
+	
 	
 	@GetMapping(path = "/hmgm/transcript-editor", produces = "application/json")
-	public @ResponseBody HmgmResourceResponse transcriptEditor(String workflowId, Long primaryfileId, String stepId) {	
-		HmgmResourceResponse response = new HmgmResourceResponse();
-		response.setMediaUrl("http://test.com/test.mp4");		
+	public @ResponseBody String transcriptEditor(String datasetPath) {	
+		
 
-    	String datasetId = null;
-    	 
-		List<Invocation> invocations = jobService.listJobs(workflowId, primaryfileId);
-    	if(invocations.size()>0) {
-    		Invocation invocation = invocations.get(0);
-    		
-        	InvocationDetails idetails = (InvocationDetails)jobService.getWorkflowsClient().showInvocation(workflowId, invocation.getId(), true);
-    		stepId = idetails.getSteps().get(2).getId();
-    		
-    		Set<String> keys = idetails.getSteps().get(2).getOutputs().keySet();
-    		Map<String, JobInputOutput> outputs = idetails.getSteps().get(2).getOutputs();
-    		for(String key : keys){
-        		datasetId = idetails.getSteps().get(2).getOutputs().get(key).getId();
-    		}
-    		
-        	Dataset dataset = jobService.showJobStepOutput(workflowId, invocation.getId(), stepId, datasetId);
-        	
-        	response.setResourceUrl(dataset.getFullDownloadUrl());
-        	
-    	}
+		galaxyInstance = galaxyApiService.getGalaxyInstance();
+		librariesClient = galaxyInstance.getLibrariesClient();
+		HistoriesClient historiesClient = galaxyInstance.getHistoriesClient();
+		try {
+
+			History matchingHistory = null;
+			List<History> histories = historiesClient.getHistories();
+			for (History curr : histories)
+			{
+				if (SHARED_HISTORY_NAME.equals(curr.getName()))
+				{
+					matchingHistory = curr;
+				}
+			}
+			
+			if(matchingHistory!=null) {
+
+				// gets all contents stored within this library
+				// converts to HashMap mapping file name to Galaxy library object since I find it easier to work with
+				List<HistoryContents> libraryContentsList = historiesClient.showHistoryContents(matchingHistory.getId());
+				for(HistoryContents content : libraryContentsList) {
+					System.out.println(content.getName() + ":" + content.getUrl());
+				}
+			}
+			
+		}
+		catch(Exception ex) {
+			System.out.println(ex.getMessage());
+		}
 		
-		
-		return response;
+		JSONParser parser = new JSONParser();
+		try {
+	        FileReader fileReader = new FileReader(datasetPath);
+	        JSONObject json = (JSONObject) parser.parse(fileReader);
+			return json.toJSONString();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "";
 	}
 	public void getStepOutput(String workflowId, Long primaryfileId, String stepId) {
 	}
