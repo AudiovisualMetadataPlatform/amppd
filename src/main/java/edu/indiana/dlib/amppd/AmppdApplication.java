4e0388e3d77e1dcd2f31806f5e8adb75644f696e
yingfeng-iu <yingfeng@iu.edu>
2020-02-17 15:55:57 -0500

    AMP-194: added config for loggers to console and files

--- src/main/java/edu/indiana/dlib/amppd/AmppdApplication.java
+++ src/main/java/edu/indiana/dlib/amppd/AmppdApplication.java
@@ -5,21 +5,28 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
 
 import edu.indiana.dlib.amppd.config.AmppdPropertyConfig;
 import edu.indiana.dlib.amppd.config.GalaxyPropertyConfig;
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
 //@EnableJdbcHttpSession
 @EnableJpaRepositories("edu.indiana.dlib.amppd.repository")
 @EnableConfigurationProperties({GalaxyPropertyConfig.class, AmppdPropertyConfig.class})
+@Slf4j
 public class AmppdApplication {
 	
     private static ApplicationContext applicationContext;
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(AmppdApplication.class, args);
+		log.info("AMPPD has started successfully.");
+		
+//        log.debug("Hello, I'm DEBUG message.");
+//        log.info("Hello, I'm INFO message.");
+//        log.warn("Hello, I'm WARN message.");
+//        log.error("Hello, I'm ERROR message.");
 	}
 	
     public static void displayAllBeans() {
