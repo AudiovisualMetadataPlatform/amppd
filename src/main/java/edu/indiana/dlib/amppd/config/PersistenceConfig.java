a5c29e8506f188b3d762864d1ca8cd63e0d6e070
yingfeng-iu <yingfeng@iu.edu>
2020-07-24 15:49:11 -0400

    AMP-669/AMP-827 - add bundle query by name+createdBy - add JPA config for audit provider

--- src/main/java/edu/indiana/dlib/amppd/config/PersistenceConfig.java
+++ src/main/java/edu/indiana/dlib/amppd/config/PersistenceConfig.java
@@ -1,9 +1,16 @@
 package edu.indiana.dlib.amppd.config;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import edu.indiana.dlib.amppd.service.AmpUserService;
 
 /**
  * Configuration for persistence layer.
@@ -13,7 +20,15 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @Configuration
 @EnableTransactionManagement
 @EnableJpaRepositories
-@EnableJpaAuditing
+@EnableJpaAuditing(auditorAwareRef = "auditorProvider")
 public class PersistenceConfig {
 
+	@Autowired
+	private AmpUserService userService;
+	
+    @Bean
+    public AuditorAware<String> auditorProvider() {
+        return () -> Optional.ofNullable(userService.getCurrentUsername());
+    }
+    
 }
--- src/main/java/edu/indiana/dlib/amppd/repository/BundleRepository.java
+++ src/main/java/edu/indiana/dlib/amppd/repository/BundleRepository.java
@@ -1,12 +1,20 @@
 package edu.indiana.dlib.amppd.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import edu.indiana.dlib.amppd.model.Bundle;
+import edu.indiana.dlib.amppd.model.Item;
 
 @CrossOrigin(origins = "*")
 @RepositoryRestResource(collectionResourceRel = "bundles", path = "bundles")
 public interface BundleRepository extends DataentityRepository<Bundle> {
+	
+	@Query(value = "select i from Item i where lower(i.name) like lower(concat('%', :keyword,'%')) or lower(i.description) like lower(concat('%', :keyword,'%'))")
+	List<Bundle> findByNameAndCreatedBy(String name, String createdBy);
 	
 }
