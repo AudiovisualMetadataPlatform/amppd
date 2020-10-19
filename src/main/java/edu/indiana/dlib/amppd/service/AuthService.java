38d954066de327e57b32e2ed3a4c08420f95fba6
dan <dan@portalmedia.com>
2020-10-19 16:26:21 -0500

    Removing unnecessary methods

--- src/main/java/edu/indiana/dlib/amppd/service/AuthService.java
+++ src/main/java/edu/indiana/dlib/amppd/service/AuthService.java
@@ -11,13 +11,5 @@ public interface AuthService {
 	 */
 	boolean compareAuthStrings(String authString, String userToken, String editorInput);
 	
-	/**
-	 * Check to see if it is an authorized user, first based on JWT set user, then token.
-	 * @param authString
-	 * @param userToken
-	 * @param editorInput
-	 * @return
-	 */
-	boolean isAuthorized(String authString, String userToken, String editorInput);
 
 }
