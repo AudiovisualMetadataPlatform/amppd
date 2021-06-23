6cb6aa710434d8ef9edaf4871964180540c2c093
yingfeng-iu <yingfeng@iu.edu>
2021-06-22 22:04:05 -0400

    AMP-1286: fix typo in transcript_to_webvtt WF step

--- src/main/java/edu/indiana/dlib/amppd/service/impl/WorkflowResultServiceImpl.java
+++ src/main/java/edu/indiana/dlib/amppd/service/impl/WorkflowResultServiceImpl.java
@@ -73,9 +73,10 @@ public class WorkflowResultServiceImpl implements WorkflowResultService {
 		put("adjust_segmentation_timestamps", "adjust_diarization_timestamps");
 		put("aws_comprehend", "aws_comprehend_ner");
 		put("aws_transcribe", "aws_transcribe_stt");
-		put("speech_segmenter", "ina_speech_segmenter");
+		put("speech_segmenter", "ina_speech_segmenter");		
 		put("VTTgenerator", "transcript_to_webvtt");
 		put("vtt_generator", "transcript_to_webvtt");
+		put("trasncript_to_webvtt", "transcript_to_webvtt");
 	}};
 	
 	// map between all obsolete output names to their standard current names
