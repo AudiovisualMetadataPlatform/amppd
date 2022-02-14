ae5aba1bf5fe8abf0559edd1e7a61574c6d5f206
yingfeng-iu <yingfeng@iu.edu>
2022-02-14 16:17:07 -0500

    AMP-1691

---
+++ src/main/java/edu/indiana/dlib/amppd/controller/WorkflowEditController.java
@@ -0,0 +1,13 @@
+package edu.indiana.dlib.amppd.controller;
+
+/**
+ * Controller to proxy requests related to workflow edit between AMP UI and Galaxy workflow editor.
+ * @author yingfeng
+ */
+public class WorkflowEditController {
+
+	/**
+	 * 
+	 */
+//	public 
+}
