4b2e47cff1b39a368d710fa1a7b76fe42592a25c
yingfeng-iu <yingfeng@iu.edu>
2022-04-18 13:59:02 -0400

    AMP-1844 - add new property - update security config to allow cors origin pattern

--- src/main/java/edu/indiana/dlib/amppd/config/AmppdPropertyConfig.java
+++ src/main/java/edu/indiana/dlib/amppd/config/AmppdPropertyConfig.java
@@ -31,6 +31,7 @@ public class AmppdPropertyConfig {
     @NotNull private String username; 
     @NotNull private String password; 
     @NotNull private String url;
+    @NotNull private String corsOriginPattern;
     @NotNull private Boolean auth;
     @NotNull private String jwtSecret;
 //    @NotNull private String workflowEditSecret;
