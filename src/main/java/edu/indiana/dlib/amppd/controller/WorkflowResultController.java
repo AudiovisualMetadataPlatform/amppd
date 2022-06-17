8ecffc7b0c27abcf77d66ab178c8cd508c7949c1
yingfeng-iu <yingfeng@iu.edu>
2022-06-17 09:43:54 -0400

    AMP-1960: update import

--- src/main/java/edu/indiana/dlib/amppd/controller/WorkflowResultController.java
+++ src/main/java/edu/indiana/dlib/amppd/controller/WorkflowResultController.java
@@ -11,7 +11,6 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
