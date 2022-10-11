2cb6ccb7e706e15e9c24fd8bcae77577bbe03f3b
Rimsha Khalid <rimshakhalid992@gmail.com>
2022-10-11 15:43:36 +0500

    fixed test case

--- src/test/java/edu/indiana/dlib/amppd/controller/WorkflowControllerTests.java
+++ src/test/java/edu/indiana/dlib/amppd/controller/WorkflowControllerTests.java
@@ -1,9 +1,8 @@
 package edu.indiana.dlib.amppd.controller;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
+import com.github.jmchilton.blend4j.galaxy.beans.Workflow;
+import edu.indiana.dlib.amppd.service.WorkflowService;
+import edu.indiana.dlib.amppd.util.TestHelper;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,10 +12,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import com.github.jmchilton.blend4j.galaxy.beans.Workflow;
-
-import edu.indiana.dlib.amppd.service.WorkflowService;
-import edu.indiana.dlib.amppd.util.TestHelper;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @AutoConfigureMockMvc
@@ -46,7 +44,7 @@ public class WorkflowControllerTests {
     @Test
     public void shouldListWorkflows() throws Exception {    	
     	mvc.perform(get("/workflows").header("Authorization", "Bearer " + token)).andExpect(status().isOk()).andExpect(
-    			jsonPath("$[0]").exists());
+    			jsonPath("$.rows[0]").exists());
     }
 
     @Test
