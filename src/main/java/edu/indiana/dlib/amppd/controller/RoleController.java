200e2e2cc4ba8dbaa67d61c1bb4073526b39e80f
yingfeng-iu <yingfeng@iu.edu>
2023-03-10 02:51:28 -0500

    AMP-2527: add API for active users query

---
+++ src/main/java/edu/indiana/dlib/amppd/controller/RoleController.java
@@ -0,0 +1,16 @@
+package edu.indiana.dlib.amppd.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+
+/**
+ * Controller to handle requests for access control.
+ * @author yingfeng
+ */
+@RestController
+@Slf4j
+public class RoleController {
+
+}
