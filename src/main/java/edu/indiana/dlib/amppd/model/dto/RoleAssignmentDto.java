19d6b175e2a8d87f8bf19f599a75d2165ef5be73
yingfeng-iu <yingfeng@iu.edu>
2023-03-23 00:36:40 -0400

    AMP-2486: - add property for unit role names - rename role assignment controller/service

--- src/main/java/edu/indiana/dlib/amppd/model/dto/RoleAssignmentDto.java
+++ src/main/java/edu/indiana/dlib/amppd/model/dto/RoleAssignmentDto.java
@@ -1,6 +1,7 @@
 package edu.indiana.dlib.amppd.model.dto;
 
 import edu.indiana.dlib.amppd.model.ac.RoleAssignment;
+import edu.indiana.dlib.amppd.model.projection.RoleAssignmentDetail;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -31,5 +32,16 @@ public class RoleAssignmentDto {
     	roleName = ra.getRole().getName();
     	unitName = ra.getUnit() == null ? null : ra.getUnit().getName();
     }
+    
+    public RoleAssignmentDto(RoleAssignmentDetail rad) {
+    	id = rad.getId();
+    	userId = rad.getUser().getId();
+    	roleId = rad.getRole().getId();
+    	unitId = rad.getUnit() == null ? null : rad.getUnit().getId();
+    	username = rad.getUser().getUsername();
+    	roleName = rad.getRole().getName();
+    	unitName = rad.getUnit() == null ? null : rad.getUnit().getName();
+    }
+    
     
 }
---
+++ src/main/java/edu/indiana/dlib/amppd/service/RoleAssignService.java
@@ -0,0 +1,48 @@
+package edu.indiana.dlib.amppd.service;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestParam;
+
+import edu.indiana.dlib.amppd.model.projection.RoleBrief;
+import edu.indiana.dlib.amppd.web.RoleAssignTable;
+import edu.indiana.dlib.amppd.web.RoleAssignTuple;
+import edu.indiana.dlib.amppd.web.RoleAssignUpdate;
+
+/**
+ * Service for role and role assignment related operations.
+ * @author yingfeng
+ */
+public interface RoleAssignService {
+	
+	/**
+	 * Get the role assignment level threshold (excluding) for the current user in the given unit, based on the rules that
+	 * a user can only assign roles weaker (with greater level value) than his strongest role (with least level value).  
+	 * @param unitId ID of the given unit
+	 * @return role assignment level threshold 
+	 */
+	public Integer getAssignableRoleLevel(Long unitId);
+	
+	/**
+	 * Get all the roles the current user has permission to assign users to, within the given unit.
+	 * @unitId ID of the unit associated with the role assignment
+	 * @return the set of roles assignable by the current user
+	 */
+	public List<RoleBrief> getAssignableRoles(@RequestParam Long unitId);
+
+	/**
+	 * Get the users, roles, and assignment info for the current user and the given unit.
+	 * @unitId ID of the unit associated with the role assignment
+	 * @return the user-role assignment table 
+	 */
+	public RoleAssignTable retrieveRoleAssignments(Long unitId);
+
+	/**
+	 * Update the given role assignments within the given unit.
+	 * @unitId ID of the given unit
+	 * assignments list of user-role-assignment 
+	 * @return the list of added/deleted RoleAssignments
+	 */
+	public RoleAssignUpdate updateRoleAssignments(Long unitId, List<RoleAssignTuple> assignments);
+	
+}
