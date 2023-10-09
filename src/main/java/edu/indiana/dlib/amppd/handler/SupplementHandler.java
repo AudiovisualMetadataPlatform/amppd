6ee561e6f5eeb5042651f7918c6efcdbea22078e
yingfeng <yingfeng@iu.edu>
2023-10-09 09:53:01 -0400

    AMP-2366 - add AC to data entity handlers

--- src/main/java/edu/indiana/dlib/amppd/handler/SupplementHandler.java
+++ src/main/java/edu/indiana/dlib/amppd/handler/SupplementHandler.java
@@ -3,18 +3,20 @@ package edu.indiana.dlib.amppd.handler;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.rest.core.annotation.HandleAfterCreate;
 import org.springframework.data.rest.core.annotation.HandleAfterDelete;
 import org.springframework.data.rest.core.annotation.HandleAfterSave;
-import org.springframework.data.rest.core.annotation.HandleBeforeCreate;
 import org.springframework.data.rest.core.annotation.HandleBeforeDelete;
 import org.springframework.data.rest.core.annotation.HandleBeforeSave;
 import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
 
 import edu.indiana.dlib.amppd.model.Supplement;
+import edu.indiana.dlib.amppd.model.ac.Action.ActionType;
+import edu.indiana.dlib.amppd.model.ac.Action.TargetType;
 import edu.indiana.dlib.amppd.service.FileStorageService;
+import edu.indiana.dlib.amppd.service.PermissionService;
 import lombok.extern.slf4j.Slf4j;
 
 
@@ -31,30 +33,43 @@ public class SupplementHandler {
 	@Autowired
 	private FileStorageService fileStorageService;
 
-    @HandleBeforeCreate
-//    @Validated({WithReference.class, WithoutReference.class})
-    public void handleBeforeCreate(@Valid Supplement supplement){
-    	// This method is needed to invoke validation before DB persistence.   	        
-    	log.info("Creating supplement " + supplement.getName() + "...");
-    }
+	@Autowired
+	private PermissionService permissionService;
+	
 
-    @HandleAfterCreate
-    public void handleAfterCreate(Supplement supplement){
-		// ingest media file after supplement is saved
-    	if (supplement.getMediaFile() != null) {
-    		fileStorageService.uploadAsset(supplement, supplement.getMediaFile());
-    	}
-    	else {
-//    		throw new RuntimeException("No media file is provided for the collectionSupplement to be created.");
-    		log.warn("No media file is provided for the supplement to be created.");
-    	}
-    	
-    	log.info("Successfully created supplement " + supplement.getId());
-    }
+	// This method is not needed anymore since the corresponding API is disabled.
+//    @HandleBeforeCreate
+////    @Validated({WithReference.class, WithoutReference.class})
+//    public void handleBeforeCreate(@Valid Supplement supplement){
+//    	// This method is needed to invoke validation before DB persistence.   	        
+//    	log.info("Creating supplement " + supplement.getName() + "...");
+//    }
+
+	// This method is not needed anymore since the corresponding API is disabled.
+//    @HandleAfterCreate
+//    public void handleAfterCreate(Supplement supplement){
+//		// ingest media file after supplement is saved
+//    	if (supplement.getMediaFile() != null) {
+//    		fileStorageService.uploadAsset(supplement, supplement.getMediaFile());
+//    	}
+//    	else {
+////    		throw new RuntimeException("No media file is provided for the collectionSupplement to be created.");
+//    		log.warn("No media file is provided for the supplement to be created.");
+//    	}
+//    	
+//    	log.info("Successfully created supplement " + supplement.getId());
+//    }
     
     @HandleBeforeSave
 //    @Validated({WithReference.class, WithoutReference.class})
     public void handleBeforeUpdate(@Valid Supplement supplement){
+		// check permission
+		Long acUnitId = supplement.getAcUnitId();
+		boolean can = permissionService.hasPermission(ActionType.Update, TargetType.Supplement, acUnitId);
+		if (!can) {
+			throw new AccessDeniedException("The current user cannot update supplements in unit " + acUnitId);
+		}
+		
     	log.info("Updating supplement " + supplement.getId() + "...");
 
         // Below file system operations should be done before the data entity is updated, 
@@ -77,6 +92,13 @@ public class SupplementHandler {
     
     @HandleBeforeDelete
     public void handleBeforeDelete(Supplement supplement) {
+		// check permission
+		Long acUnitId = supplement.getAcUnitId();
+		boolean can = permissionService.hasPermission(ActionType.Delete, TargetType.Supplement, acUnitId);
+		if (!can) {
+			throw new AccessDeniedException("The current user cannot delete supplements in unit " + acUnitId);
+		}
+		
         log.info("Deleting supplement " + supplement.getId() + " ...");
 
         // Below file system operations should be done before the data entity is deleted, so that 
--- src/main/java/edu/indiana/dlib/amppd/handler/UnitHandler.java
+++ src/main/java/edu/indiana/dlib/amppd/handler/UnitHandler.java
@@ -10,12 +10,16 @@ import org.springframework.data.rest.core.annotation.HandleBeforeCreate;
 import org.springframework.data.rest.core.annotation.HandleBeforeDelete;
 import org.springframework.data.rest.core.annotation.HandleBeforeSave;
 import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
 
 import edu.indiana.dlib.amppd.model.Unit;
+import edu.indiana.dlib.amppd.model.ac.Action.ActionType;
+import edu.indiana.dlib.amppd.model.ac.Action.TargetType;
 import edu.indiana.dlib.amppd.service.DropboxService;
 import edu.indiana.dlib.amppd.service.FileStorageService;
+import edu.indiana.dlib.amppd.service.PermissionService;
 import lombok.extern.slf4j.Slf4j;
 
 
@@ -34,12 +38,22 @@ public class UnitHandler {
 
 	@Autowired
 	private DropboxService dropboxService;
+
+	@Autowired
+	private PermissionService permissionService;
+
 	
 	@HandleBeforeCreate
     public void handleBeforeCreate(@Valid Unit unit) {        
-    	 // This method is needed to invoke validation before DB persistence.
-    	 // We don't need to create dropbox sub-directory when creating a unit, as that can be handled when a collection is created.    	
-    	log.info("Creating unit " + unit.getName() + "...");
+		// check permission
+		boolean can = permissionService.hasPermission(ActionType.Create, TargetType.Unit, null);
+		if (!can) {
+			throw new AccessDeniedException("The current user cannot create units.");
+		}
+
+		// This method is needed to invoke validation before DB persistence.
+		// We don't need to create dropbox sub-directory when creating a unit, as that can be handled when a collection is created.    	
+		log.info("Creating unit " + unit.getName() + "...");
     }
 
     @HandleAfterCreate
@@ -49,6 +63,13 @@ public class UnitHandler {
     
     @HandleBeforeSave
     public void handleBeforeUpdate(@Valid Unit unit) {
+		// check permission
+		Long acUnitId = unit.getAcUnitId();
+		boolean can = permissionService.hasPermission(ActionType.Update, TargetType.Unit, acUnitId);
+		if (!can) {
+			throw new AccessDeniedException("The current user cannot update unit " + acUnitId);
+		}
+		
     	log.info("Updating unit " + unit.getName() + "...");
 
         // Below file system operations should be done before the data entity is updated, 
@@ -70,6 +91,13 @@ public class UnitHandler {
     
     @HandleBeforeDelete
     public void handleBeforeDelete(Unit unit) {
+		// check permission
+		Long acUnitId = unit.getAcUnitId();
+		boolean can = permissionService.hasPermission(ActionType.Delete, TargetType.Unit, acUnitId);
+		if (!can) {
+			throw new AccessDeniedException("The current user cannot delete unit " + acUnitId);
+		}
+		
         log.info("Deleting unit " + unit.getId() + " ...");
 
         // Below file system operations should be done before the data entity is deleted, so that
