03494af7512dc5da632c207dc89da0995089e52b
yingfeng <yingfeng@iu.edu>
2024-06-26 16:03:49 -0400

    AMP-3079: improve error msg for account activation

--- src/main/java/edu/indiana/dlib/amppd/service/impl/AmpUserServiceImpl.java
+++ src/main/java/edu/indiana/dlib/amppd/service/impl/AmpUserServiceImpl.java
@@ -339,7 +339,7 @@ public class AmpUserServiceImpl implements AmpUserService, UserDetailsService {
 			else if (user.getStatus() == AmpUser.Status.ACTIVATED) {
 				response.setSuccess(false);
 				response.addError("Cannot activate user account: it's already activated!");
-				log.error("Cannot activate user account: it's already activated: " + user.getUsername());
+				log.error("No need to activate user account: it's already activated: " + user.getUsername());
 			}
 			else if (user.getStatus() == AmpUser.Status.ACCEPTED) {
 				try {
